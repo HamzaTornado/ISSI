04de4d887a636118b2b91ae2ceac671cced5618b
Dell <Dell@DESKTOP-J4OBT5F>
2020-05-28 12:26:33 +0000

    First commit

---
+++ ISSI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISSI.Startup))]
+namespace ISSI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
